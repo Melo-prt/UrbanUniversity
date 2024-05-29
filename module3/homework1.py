a7b948ff07e7272e34568e9eefc7c55afdd8b1b1
Melo_prt <169174796+Melo-prt@users.noreply.github.com>
2024-05-29 10:24:41 +0300

    ПЗ "Пространство имен и способы вызова функции" 29.05.2024

--- module3/homework1.py
+++ module3/homework1.py
@@ -1,12 +1,20 @@
-words = input('Введите проверяемые слова через пробел: ').split()
-root = input('Введите корень слова: ')
-root_words = []
+words_1 = input('Введите проверяемые слова через пробел: ').split()
+root_1 = input('Введите первый корень: ')
+words_2 = input('Введите проверяемые слова через пробел: ').split()
+root_2 = input('Введите второй корень: ')
+root_words_1 = []
+root_words_2 = []
 
 def single_root_words():
-    for word in words:
-        is_word = root.lower() in word.lower()
-        if is_word == True:
-            root_words.append(word)
-    print('Однокоренные слова:', *root_words)
+    for word in words_1:
+        is_word_1 = root_1.lower() in word.lower()
+        if is_word_1 == True:
+            root_words_1.append(word)
+    for word in words_2:
+        is_word_2 = root_2.lower() in word.lower()
+        if is_word_2 == True:
+            root_words_2.append(word)
+    print(*root_words_1)
+    print(*root_words_2)
 
 single_root_words()
